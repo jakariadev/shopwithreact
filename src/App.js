9eb8688db963de834229a25ee3fa8d6946c2a6d8
Jakaria <jakaria.pust@gmail.com>
2021-01-31 06:45:46 -0600

    Dynamic Routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import {Switch, Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 
 import './App.css';
 import Homepage from './pages/homepage/home.component.jsx';
 
-const HatsPage = () =>(
-  <div>
-    <h1> HATS</h1>
-  </div>
-)
+const TopicList = (props) =>{
+  console.log(props);
+  return (
+    <div>
+      <button onClick={()=> props.history.push('/topics/13')}>Button1</button>
+      <h1> TopicList Page</h1>
+      <Link to={`${props.match.url}/13`}>Link 13</Link>
+      <Link to={`${props.match.url}/14`}>Link 14</Link>
+      <Link to={`${props.match.url}/17`}>Link 17</Link>
+    </div>
+  );
+  
+};
+const TopicDetail = (props) =>{
+  console.log(props);
+  return (
+    <div>
+    <Link to='/topics'> Topic List</Link>
+      <h1> TopicDetail Page: {props.match.params.topicId}</h1>
+    </div>
+  );
+  
+};
 function App() {
   return (
     <div className="homepage">
-    <Switch>
       <Route exact path='/' component={Homepage} />
-      <Route path='/hats' component={HatsPage} />
-      </Switch>
+      <Route exact path='/topics' component={TopicList} />
+      <Route exact path='/topics/:topicId' component={TopicDetail} />
+      <Route exact path='/pag2/topics' component={TopicList} />
+      <Route exact path='/pag2/topics/:topicId' component={TopicDetail} />
+     
     </div>
   );
 }
