6157c8df0a23f97b0df8d208ed811a131ed5fa9a
Jakaria <jakaria.pust@gmail.com>
2021-02-05 12:37:10 -0600

    Shop updated with shop page, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
 import Homepage from './pages/homepage/home.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage = () =>(
-  <div>
-    <h1> HATS</h1>
-  </div>
-)
-const Jacketpage = () =>(
-  <div>
-    <h1> Jacketpage </h1>
-  </div>
-)
-const Sneakerspage = () =>(
-  <div>
-    <h1> sneakers Page </h1>
-  </div>
-)
-const Womenspage = () =>(
-  <div>
-    <h1> womens Page </h1>
-  </div>
-)
-const Menspage = () =>(
-  <div>
-    <h1> mens Page </h1>
-  </div>
-)
+
 function App() {
   return (
-    <div className="homepage">
-      <Route exact path='/' component={Homepage} />
-      <Route path='/hats' component={HatsPage} />
-      <Route path='/jackets' component={Jacketpage} />
-      <Route path='/sneakers' component={Sneakerspage} />
-      <Route path='/womens' component={Womenspage} />
-      <Route path='/mens' component={Menspage} />
-     
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
