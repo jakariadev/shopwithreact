49a6216ff0a7cc32eb6cc09c2a2326f7f7a22ed1
Jakaria <jakaria.pust@gmail.com>
2021-02-09 01:14:00 -0600

    Sign iN , form, custom button component added in signin url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import Homepage from './pages/homepage/home.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
-
+import SignINAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import Header from './components/header/header.component.jsx';
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignINAndSignUpPage} />
       </Switch>
     </div>
   );
